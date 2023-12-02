4d8b1351cba288078f6726434146a57bda9acf16
Devin Collins <me@imdevinc.com>
2023-12-01 23:27:58 -0800

    Day 2 - Part 2

--- day2/main.go
+++ day2/main.go
@@ -24,42 +24,44 @@ func main() {
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 
-	validCount := 0
-	id := 0
+	sum := 0
 	for fileScanner.Scan() {
-		id++
 		input := fileScanner.Text()
-		if validateLine(input) {
-			validCount += id
-		}
+		colors := validateLine(input)
+		sum += (colors[0] * colors[1] * colors[2])
 	}
-	log.Println("Valid pulls:", validCount)
+
+	log.Println(sum)
 }
 
-func validateLine(input string) bool {
+func validateLine(input string) []int {
+	minBlue := 0
+	minRed := 0
+	minGreen := 0
+
 	blueMatches := blueRe.FindAllStringSubmatch(input, -1)
 	for _, match := range blueMatches {
 		val, _ := strconv.Atoi(match[1])
-		if val > valid["blue"] {
-			return false
+		if val > minBlue {
+			minBlue = val
 		}
 	}
 
 	redMatches := redRe.FindAllStringSubmatch(input, -1)
 	for _, match := range redMatches {
 		val, _ := strconv.Atoi(match[1])
-		if val > valid["red"] {
-			return false
+		if val > minRed {
+			minRed = val
 		}
 	}
 
 	greenMatches := greenRe.FindAllStringSubmatch(input, -1)
 	for _, match := range greenMatches {
 		val, _ := strconv.Atoi(match[1])
-		if val > valid["green"] {
-			return false
+		if val > minGreen {
+			minGreen = val
 		}
 	}
 
-	return true
+	return []int{minRed, minBlue, minGreen}
 }
